eb111739404a4bf543e1aaca364e875023d9a846
TimKova <72708781+TimKova@users.noreply.github.com>
2024-10-11 03:23:30 -0400

    Update State_Manager.cs
    Added rudimentary hammer -> hand function. Would require further revision to accurately place it in hand and correct animation so that it does not actively obscure player view.

--- Phorge/Assets/Scripts/State_Manager.cs
+++ Phorge/Assets/Scripts/State_Manager.cs
@@ -40,6 +40,7 @@ public class State_Manager : MonoBehaviour
         }
 
         taskInteraction();
+        hammerInteraction();
         player_manager.GetComponent<Player_Manager>().set_cur_state(state_to_be);
         player_manager.GetComponent<Player_Manager>().set_cur_task(taskName);
         //print(state_to_be);
@@ -60,6 +61,24 @@ public class State_Manager : MonoBehaviour
         }
     }
 
+    void hammerInteraction()
+    {
+        if (inHammerRange && Input.GetKeyDown(KeyCode.Q))
+        {
+            print("attemping item pickup");
+            GameObject hammer = GameObject.FindWithTag("Hammer");
+            GameObject hands = GameObject.FindWithTag("Hands");
+            GameObject test = GameObject.FindWithTag("Test");
+            hammer.transform.position = hands.transform.position;
+            print(hammer.transform.position);
+            Vector3 offset = new Vector3(0.3f, -0.03f, -0.02f);
+            hammer.transform.position = test.transform.position + offset;
+            print(hammer.transform.position);
+            //hammer.transform.rotation = Quaternion.Euler((float)-77.138, (float)272.87, (float)23.359);
+            hammer.transform.parent = hands.transform;
+        }
+    }
+
     private void OnTriggerEnter(Collider other) // to see when the player enters the collider
     {
         //print("Trigger Enter");
@@ -68,6 +87,11 @@ public class State_Manager : MonoBehaviour
             inTaskRange = true;
             taskName = other.name;
             print("In Task Range of " + taskName);
+        }
+        else if (other.gameObject.tag == "Hammer")
+        {
+            print("In Range of Hammer");
+            inHammerRange = true;
         }
         else if (other.gameObject.tag == "NPC")
         {
